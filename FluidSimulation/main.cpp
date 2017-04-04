9135924abbe9e35446731ab4d7834073bd8941e7
prwang <bbcc293@hotmail.com>
2017-04-05 00:28:22 +0800

    3d-通过编译

--- FluidSimulation/main.cpp
+++ FluidSimulation/main.cpp
@@ -4,26 +4,26 @@
 FluidSimulation fluidsim;
 
 void Step_Time(int value) {
-	printf("step time value: %d\n", value);
-	fluidsim.Step_Time();
-	glutPostRedisplay();
-	glutTimerFunc(1000/FPS, Step_Time, 1);
+    printf("step time value: %d\n", value);
+    fluidsim.Step_Time();
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS, Step_Time, 1);
 }
 
 void Display_Func(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	fluidsim.Draw_On_Screen();
-	glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT);
+    fluidsim.Draw_On_Screen();
+    glutSwapBuffers();
 }
 int main(int argc, char *argv[])
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);//double buffer
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(SHOW_SIZE_X*2, SHOW_SIZE_Y);
-	int glut_window = glutCreateWindow("Fluid Simulation");
-	glutDisplayFunc(&Display_Func);
-	glutTimerFunc(1000/FPS, Step_Time, 1);
-	glutMainLoop();
-	return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);//double buffer
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(SHOW_SIZE_X*2, SHOW_SIZE_Y);
+    int glut_window = glutCreateWindow("Fluid Simulation");
+    glutDisplayFunc(&Display_Func);
+    glutTimerFunc(1000/FPS, Step_Time, 1);
+    glutMainLoop();
+    return 0;
 }
--- FluidSimulation/shared.h
+++ FluidSimulation/shared.h
@@ -6,11 +6,17 @@
 #include <cstring>
 #include <cmath>
 #include <cassert>
+#include <vector>
+#include <list>
 #include <GL/glut.h>
 using namespace std;
 
 typedef char BYT;
 typedef float Float;
+
+template<class T> constexpr T EPS = 0;
+template<> constexpr float EPS<float> = 1e-3;
+template<> constexpr double EPS<double> = 1e-9;
 
 const int FPS = 100;
 
@@ -28,28 +34,28 @@ const int GRID_SIZE_Y = 64;
 const int GRID_SIZE_Z = 64;
 
 inline int ID(int x, int y, int z) {
-	assert(0 <= x&&x < GRID_SIZE_X);
-	assert(0 <= y&&y < GRID_SIZE_Y);
-	assert(0 <= z&&z < GRID_SIZE_Z);
-	return x*GRID_SIZE_Y*GRID_SIZE_Z + y*GRID_SIZE_Z + z;
+    assert(0 <= x&&x < GRID_SIZE_X);
+    assert(0 <= y&&y < GRID_SIZE_Y);
+    assert(0 <= z&&z < GRID_SIZE_Z);
+    return x*GRID_SIZE_Y*GRID_SIZE_Z + y*GRID_SIZE_Z + z;
 }
 
 inline int random(int x, int y) {
-	return rand() % (y - x + 1) + x;
+    return rand() % (y - x + 1) + x;
 }
 
 inline Float randomF() {
-	return random(0, 9999) / (10000.0);
+    return random(0, 9999) / (10000.0);
 }
 
 inline bool validX(int x) {
-	return 0 <= x&&x < GRID_SIZE_X;
+    return 0 <= x&&x < GRID_SIZE_X;
 }
 
 inline bool validY(int y) {
-	return 0 <= y&&y < GRID_SIZE_Y;
+    return 0 <= y&&y < GRID_SIZE_Y;
 }
 
 inline bool validZ(int z) {
-	return 0 <= z&&z < GRID_SIZE_Z;
+    return 0 <= z&&z < GRID_SIZE_Z;
 }
