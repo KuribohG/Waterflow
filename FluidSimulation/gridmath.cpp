2e8297c56ecf41cb677cd3472319fad660cba525
wmdcstdio <cstdio@sina.cn>
2017-05-04 22:45:19 +0800

    delete useless comments

--- FluidSimulation/gridmath.cpp
+++ FluidSimulation/gridmath.cpp
@@ -2,11 +2,6 @@
 
 
 
-/*template<typename T>
-T& Array3D<T>::operator[](int i){
-	return f[i];
-}*/
-
 Float Neighbor_Sum6(aryf &x, int i, int j, int k) {
 	Float tmp = 0;
 	if (i - 1 >= 0) tmp += x(i - 1, j, k);
@@ -45,25 +40,3 @@ Float Interpolation_Water_Velocity(int axis, const aryf &f, Float x, Float y, Fl
 	if (sw == 0) return 0;
 	return v / sw;
 }
-
-/*Float Interpolation_In_Water_3D(aryf &f, Float x, Float y, Float z, aryi &mask) {
-	// todo: somehow re-write all interpolation scenes to take 0.5 offset into consideration
-	//x -= 0.5, y -= 0.5, z -= 0.5;
-	int x0 = floor(x), x1 = x0 + 1;
-	int y0 = floor(y), y1 = y0 + 1;
-	int z0 = floor(z), z1 = z0 + 1;
-	Float s0 = x - x0, s1 = x1 - x;
-	Float u0 = y - y0, u1 = y1 - y;
-	Float v0 = z - z0, v1 = z1 - z;
-	Float v = 0, sw = 0;
-	if (Valid_Water(x0, y0, z0, mask)) v += s1*u1*v1*f(x0, y0, z0), sw += s1*u1*v1;
-	if (Valid_Water(x0, y0, z0, mask)) v += s1*u1*v0*f(x0, y0, z1), sw += s1*u1*v0;
-	if (Valid_Water(x0, y0, z0, mask)) v += s1*u0*v1*f(x0, y1, z0), sw += s1*u0*v1;
-	if (Valid_Water(x0, y0, z0, mask)) v += s1*u0*v0*f(x0, y1, z1), sw += s1*u0*v0;
-	if (Valid_Water(x0, y0, z0, mask)) v += s0*u1*v1*f(x1, y0, z0), sw += s0*u1*v1;
-	if (Valid_Water(x0, y0, z0, mask)) v += s0*u1*v0*f(x1, y0, z1), sw += s0*u1*v0;
-	if (Valid_Water(x0, y0, z0, mask)) v += s0*u0*v1*f(x1, y1, z0), sw += s0*u0*v1;
-	if (Valid_Water(x0, y0, z0, mask)) v += s0*u0*v0*f(x1, y1, z1), sw += s0*u0*v0;
-	if (sw == 0) return 0;
-	return v / sw;
-}*/
