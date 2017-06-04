d41deeee044cd845f51ab5bbf1de087112cb0431
wmdcstdio <cstdio@sina.cn>
2017-06-04 11:39:20 +0800

    fix server code, add run py

--- FluidSimulation/run_simulation.py
+++ FluidSimulation/run_simulation.py
@@ -1,6 +1,11 @@
 import os
 import sys
+os.system("chmod +w /unsullied/sharefs/wangmengdi/wangmengdi_EMC")
 
 if __name__ == "__main__":
-	filename = sys.argv[1]
-	dumppref = 
+	boxname = sys.argv[1]
+	basename = boxname.split("/")[-1].rstrip(".box")
+	dumppref = "objs/%s"%basename
+	os.system("mkdir -p %s"%dumppref)
+	print boxname,dumppref
+	os.system("rlaunch --cpu=1 --gpu=0 --memory=8192 -- ./FluidSimulation %s %s"%(boxname,dumppref))
